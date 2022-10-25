af11b71ac0ec180d50793244f04cfa1dae0c7e2d
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-10-25 12:20:03 -0700

    add code to project

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+ apiKey: "AIzaSyBp7W8iv5fgxQgrpfXFhKfgKB12SqN03xQ",
+  authDomain: "fir-ca432.firebaseapp.com",
+  projectId: "fir-ca432",
+  storageBucket: "fir-ca432.appspot.com",
+  messagingSenderId: "1086739998382",
+  appId: "1:1086739998382:web:d21460935b287b0df1af3f"
 };
 
 //--------------------------------------------
